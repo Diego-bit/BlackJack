ec2860aeeeed1b91f9e2948db8fba5ee63f75353
Diego-bit <ugartediegoedgar@gmail.com>
2020-10-10 19:26:21 -0500

    pedir carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,7 +1,14 @@
-
 let deck = [];
 const tipos = ["C", "D", "H", "S"];
 const especiales = ["A", "Q", "J", "K"];
+
+let puntosJugador = 0,
+    puntosComputadora = 0;
+
+//referencias del HTML
+
+const btnPedir = document.querySelectos('#btnPedir');
+const puntosHTML = document.querySelectorAll('small');
 
 const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
@@ -16,9 +23,7 @@ const crearDeck = () => {
     }
   }
 
-  
-  deck = _.shuffle(deck);
-  console.log(deck);
+  sdeck = _.shuffle(deck);
   return deck;
 };
 
@@ -27,23 +32,32 @@ crearDeck();
 //esta funcion permite tomar una carta
 
 const pedirCarta = () => {
-
-    if ( deck.length === 0){
-        throw 'No hay cartas en el deck';
-    }
-    const carta = deck.pop();
-    console.log(deck);
-    console.log(carta); // carta debe ser la baraja
-    return carta;
-}
+  if (deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
+  const carta = deck.pop();
+  console.log(deck);
+  console.log(carta); // carta debe ser la baraja
+  return carta;
+};
 
 //    pedirCarta();
 
-const valorCarta = (  carta) => {
-    const valor = carta.substring (0, carta.length - 1);
-return (isNaN(valor))?
-    (valor === 'A') ? 11 : 10
-    : valor * 1;
-}
+const valorCarta = (carta) => {
+  const valor = carta.substring(0, carta.length - 1);
+  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+};
 const valor = valorCarta(pedirCarta());
-console.log({valor});
+console.log({ valor });
+
+// eventos
+
+btnPedir.addEventListener('click', () => {
+
+  const carta = pedirCarta();
+
+    puntosJugador = puntosComputadora + valorCarta( carta );
+    puntosHTML[0].innerText = puntosJugador;
+
+
+});
