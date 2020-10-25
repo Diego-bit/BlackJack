1d3cea01597ebc0994a0d0177941af5a94c43ad9
Diego-bit <ugartediegoedgar@gmail.com>
2020-10-24 19:03:22 -0500

    crear cartas en html

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -7,7 +7,8 @@ let puntosJugador = 0,
 
 //referencias del HTML
 
-const btnPedir = document.querySelectos('#btnPedir');
+const btnPedir = document.querySelector('#btnPedir');
+const divCartaJugador = document.querySelector('#jugador-cartas');
 const puntosHTML = document.querySelectorAll('small');
 
 const crearDeck = () => {
@@ -23,7 +24,7 @@ const crearDeck = () => {
     }
   }
 
-  sdeck = _.shuffle(deck);
+  deck = _.shuffle(deck);
   return deck;
 };
 
@@ -56,8 +57,24 @@ btnPedir.addEventListener('click', () => {
 
   const carta = pedirCarta();
 
-    puntosJugador = puntosComputadora + valorCarta( carta );
+    puntosJugador = puntosJugador + valorCarta( carta );
     puntosHTML[0].innerText = puntosJugador;
+
+    //          <img class="carta" src="assets/cartas/10C.png" alt="" />
+
+    const imgCarta = document.createElement('img');
+    imgCarta.src = `assets/cartas/${carta}.png`;
+    imgCarta.classList.add('carta');
+
+    divCartaJugador.append(imgCarta);
+
+    if(puntosJugador >21){
+      console.warn('Perdiste');
+      btnPedir.disabled=true;
+    } else if(puntosJugador == 21 ){
+      console.warn('ganaste')
+      btnPedir.disabled=true;
+    }
 
 
 });
